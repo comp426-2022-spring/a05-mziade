aa4f8c128231fb9415aa60f654006a40dfce16e8
Mia Ziade <mziade@live.unc.edu>
2022-04-18 11:35:09 -0400

    button references

--- public/main.js
+++ public/main.js
@@ -1,7 +1,11 @@
 // Focus div based on nav button click
+var home = document.getElementById("homenav");
+var single = document.getElementById("singlenav");
+var multi = document.getElementById("multinav");
+var guess = document.getElementById("guessnav");
 
 // Flip one coin and show coin image to match result when button clicked
-
+if(button#)
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
