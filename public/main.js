009d5fc54b4102c7b3925fd703f6dd00ab0f0459
Mia Ziade <mziade@live.unc.edu>
2022-04-20 10:28:52 -0400

    j

--- public/main.js
+++ public/main.js
@@ -11,7 +11,6 @@ const home = document.getElementById("homenav")
 const singlenav = document.getElementById("singlenav")
 const multinav = document.getElementById("multinav")
 const guessnav = document.getElementById("guessnav")
-
 // Flip one coin and show coin image to match result when button clicked
 singlenav.addEventListener("click", flipCoin)
 			function flipCoin() {
@@ -30,22 +29,46 @@ singlenav.addEventListener("click", flipCoin)
 				// console.log("Coin has been flipped. Result: "+ flip)
             }
 // Flip multiple coins and show coin images in table as well as summary results
-// multi.addEventListener("click", flipCoins)
-// 			function flipCoins() {
-//                 fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
-//   				.then(function(response) {
-//     			  return response.json();
-//   				})
-// 				.then(function(result) {
-// 					console.log(result);
-// 					document.getElementById("resultmult").innerHTML = result.flip;
-// 					// document.getElementById("quarter").setAttribute("src", result.flip+".jpg");
-// 					multi.disabled = true
-// 				})
-//                 let flip = "FLIPPED"
-// 				document.getElementById("multi").innerHTML = flip;
-// 				console.log("Coins have been flipped. Result: "+ flip)
-//             }
+const coins = document.getElementById("coins")
+coins.addEventListener("submit", flipCoins)
+			// Create the submit handler
+			async function flipCoins(event) {
+				event.preventDefault();
+				
+				const endpoint = "app/flip/coins/"
+				const url = document.baseURI+endpoint
+
+				const formEvent = event.currentTarget
+
+				try {
+					const formData = new FormData(formEvent);
+					const flips = await sendFlips({ url, formData });
+
+					console.log(flips);
+					document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+					document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+				} catch (error) {
+					console.log(error);
+				}
+			}
+			// Create a data sender
+			async function sendFlips({ url, formData }) {
+				const plainFormData = Object.fromEntries(formData.entries());
+				const formDataJson = JSON.stringify(plainFormData);
+				console.log(formDataJson);
+
+				const options = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Accept: "application/json"
+					},
+					body: formDataJson
+				};
+
+				const response = await fetch(url, options);
+				return response.json()
+			}
 //Enter number and press button to activate coin flip series
 
 //Guess a flip by clicking either heads or tails button
